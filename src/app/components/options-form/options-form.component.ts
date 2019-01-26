081625e5d3a080edfbc74c5f03b5fb9c25b82900
Xurmein <wj.pavocrist@gmail.com>
2019-01-26 10:52:01 -0500

    finished enums for dice results

---
+++ src/app/components/options-form/options-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-options-form',
+  templateUrl: './options-form.component.html',
+  styleUrls: ['./options-form.component.css']
+})
+export class OptionsFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
